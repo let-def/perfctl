6225e64394d32cd926a0ab6733876d9b020454e8
Frédéric Bour <frederic.bour@lakaban.net>
2025-06-20 20:34:12 +0900

    Add README, tweak mli comments

--- lib/perfctl.mli
+++ lib/perfctl.mli
@@ -1,17 +1,33 @@
+(**
+  Module `Perfctl`: Control 'perf' profiling from within the program being profiled.
 
-(* Is the program running under perfctl? *)
+  This module provides functions to dynamically enable and disable profiling
+  using the Linux `perf` tool. It allows the program being profiled to control
+  when profiling starts and stops, enabling precise profiling of specific code sections.
+*)
+
 val is_available : bool
+(** Check if the program is running under perfctl.
 
-(* Enable profiling if [is_available] is true.
-   Otherwise, do nothing.
-
-   This call blocks while waiting for an answer from perf. *)
+    This function returns `true` if the program is being profiled by perfctl,
+    and `false` otherwise. It can be used to conditionally enable or disable
+    profiling based on the current environment.
+*)
 
 val enable : unit -> unit
+(** Enable profiling if perfctl is available.
 
-(* Disable profiling if [is_available] is true.
-   Otherwise, do nothing.
+    This function enables profiling if the program is running under perfctl.
+    If perfctl is not available, this function does nothing.
 
-   This call blocks while waiting for an answer from perf. *)
+    The call blocks while waiting for an answer from perf. Execution reumes only after perf confirms the request.
+*)
 
 val disable : unit -> unit
+(** Disable profiling if perfctl is available.
+
+    This function disables profiling if the program is running under perfctl.
+    If perfctl is not available, this function does nothing.
+
+    The call blocks while waiting for an answer from perf. Execution resumes only after perf confirms the request.
+*)
